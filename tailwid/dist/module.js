74b6e9ea998b17e727efdd0bf44fc81c64e81b4a
TangB5 <ndohyannick78@gmail.com>
2024-04-01 12:31:42 +0200

    page d'acceuille mis a jour et debut de la page login

---
+++ tailwid/dist/module.js
@@ -0,0 +1,24 @@
+let mob=document.querySelector(".mob");
+let liste=document.querySelector(".liste");
+let ouvert=document.querySelector(".ouvert");
+let fermer=document.querySelector(".fermer");
+let isdown=true;
+
+mob.addEventListener("click",()=>{
+  
+  if(isdown)
+  {
+  liste.classList.add("top-[12%]","opacity-100","fixed");
+  ouvert.classList.add("hidden","duration-500");
+  fermer.classList.remove("hidden");
+ 
+}
+else{
+  liste.classList.remove("top-[12%]","opacity-100","fixed");
+  ouvert.classList.remove("hidden");
+  fermer.classList.add("hidden","duration-200");
+}
+
+isdown=!isdown;
+
+});
