1e781b49b72fdff977d1b768195ac1aaaec84781
TangB5 <ndohyannick78@gmail.com>
2024-04-10 05:04:49 +0200

    mis ajour de tous les page

--- tailwid/dist/module.js
+++ tailwid/dist/module.js
@@ -8,13 +8,13 @@ mob.addEventListener("click",()=>{
   
   if(isdown)
   {
-  liste.classList.add("top-[15%]","opacity-100");
+  liste.classList.add("top-[12%]","opacity-100");
   ouvert.classList.add("hidden","duration-500");
   fermer.classList.remove("hidden");
  
 }
 else{
-  liste.classList.remove("top-[15%]","opacity-100");
+  liste.classList.remove("top-[12%]","opacity-100");
   ouvert.classList.remove("hidden");
   fermer.classList.add("hidden","duration-200");
 }
